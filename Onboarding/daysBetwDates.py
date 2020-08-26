92bb90cea4c6f6f2d5f7230c7778426450b873a7
CodeSoju <wolfgangliszt1984@gmail.com>
2020-08-26 00:34:10 -0600

    Finished daysBetweenDates

--- Onboarding/daysBetwDates.py
+++ Onboarding/daysBetwDates.py
@@ -1,10 +1,29 @@
+def isLeapYear(year):
+    #from Wiki
+    if year % 400 == 0:
+        return True
+    if year % 100 == 0:
+        return False
+    if year % 4 == 0:
+        return True
+    return False
+
 #Calculating the number of days between two dates
 def daysInMonth(year, month):
+    if month == 1 or month == 3 or month ==5 or month == 7 or month == 8 or month == 10 or month == 12:
+        return 31
+    else:
+        if month == 2:
+            if isLeapYear(year):
+                return 29
+            return 28
+        else: 
+            return 30
     return 30
 
 def nextDay(year, month, day):
     '''Warning: this version assumes all months have 30 days!!!'''
-    if day < 30:
+    if day < daysInMonth(year, month):
         return year, month, day + 1
     else:
         if month < 12:
@@ -37,3 +56,19 @@ def daysBetweenDates(year1, month1, day1, year2, month2, day2):
         days += 1
     return days
     
+def test():
+    # tests with 30-day
+    assert daysBetweenDates(2013, 1, 1, 2013, 1, 1) == 0
+    assert daysBetweenDates(2013, 1, 1, 2013, 1, 2) == 1
+    assert nextDay(2013, 1, 1) == (2013, 1, 2)
+    assert nextDay(2013, 4, 30) == (2013, 5, 1)
+    assert nextDay(2012, 12, 31) == (2013, 1, 1)
+    assert nextDay(2013, 2, 28) == (2013, 3, 1)
+    assert nextDay(2013, 9, 30) == (2013, 10, 1)
+    assert nextDay(2012, 2, 28) == (2012, 2, 29)
+    assert daysBetweenDates(2012, 1, 1, 2013, 1, 1) == 366
+    assert daysBetweenDates(2013, 1,1, 2014, 1, 1) == 365
+    assert daysBetweenDates(2013, 1, 24, 2013, 6, 29) == 156
+    print('Tests finished')
+
+test()
